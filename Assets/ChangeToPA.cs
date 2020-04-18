c39027a7987da47e99b23edc96b7226db510f63a
Ryan Castelli <ryancastelli@gmail.com>
2020-04-17 23:41:25 -0400

    scene switching

---
+++ Assets/ChangeToPA.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeToPA : MonoBehaviour {
+
+	public string levelToLoad;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0)
+        {
+			Application.LoadLevel(levelToLoad);
+        }
+	}
+}
