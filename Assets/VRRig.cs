91af1ac84f247dc6440f37599fab0406f31fed45
Ryan Castelli <ryancastelli@gmail.com>
2020-04-17 20:36:21 -0400

    testing with button presses

---
+++ Assets/VRRig.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRRig : MonoBehaviour {
+
+	public bool inputA;
+	private bool prevInput = false;
+	public GameObject rCube;
+
+	// Update is called once per frame
+	void Update () {
+
+		inputA = OVRInput.Get(OVRInput.RawButton.A);
+
+		if (inputA != prevInput)
+        {
+			ButtonStateChanged();
+        }
+		
+	}
+
+	void ButtonStateChanged()
+    {
+		if (prevInput == false) //new press
+		{
+			rCube?.SetActive(false);
+		}
+
+		prevInput = inputA;
+    }
+}
